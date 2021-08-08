3f2564ea5a3e36c622a670b2d927546315694f05
backgtx <wesleyafc2010@gmail.com>
2021-08-08 16:27:12 -0300

    initial

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import express from "express";
 
 const app = express()
+app.use(express.json())
 
-app.get("/", (req, res) => {
-    return res.json({ message: "hello world" })
-})
 app.listen(3333, () => console.log("server is running"))
