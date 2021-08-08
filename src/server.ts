dd350639fbc9d296b506a67d16a40387d7a6bf95
backgtx <wesleyafc2010@gmail.com>
2021-08-08 18:04:59 -0300

    create categories routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
-import express from "express";
+import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
-const app = express()
-app.use(express.json())
+const app = express();
 
-app.listen(3333, () => console.log("server is running"))
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
+
+app.listen(3333, () => console.log('server is running'));
