28422b5e940fffaaac6c78dfcabd9610c106882f
backgtx <wesleyafc2010@gmail.com>
2021-08-08 20:44:49 -0300

    respositories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,17 @@
 import { Router } from 'express';
-import { Category } from '../model/Category';
+
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories: Category[] = [];
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  })
 
-  categories.push(category);
-
-  return response.status(201).json(categories).send();
+  categoriesRepository.create({ name, description })
+  return response.status(201).json().send();
 });
 
 export { categoriesRoutes };
